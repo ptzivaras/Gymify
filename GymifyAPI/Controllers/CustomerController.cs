af648b9513df36c485d9e4bd8b9a64de46faffdf
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-07-27 00:52:51 +0300

    Refactor Controller

--- GymifyAPI/Controllers/CustomerController.cs
+++ GymifyAPI/Controllers/CustomerController.cs
@@ -2,45 +2,46 @@ using GymifyAPI.DTOs;
 using GymifyAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GymifyAPI.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class CustomerController : ControllerBase
+namespace GymifyAPI.Controllers
 {
-    private readonly ICustomerService _service;
-
-    public CustomerController(ICustomerService service)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
     {
-        _service = service;
-    }
+        private readonly ICustomerService _service;
 
-    // GET: api/customer
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<CustomerDto>>> GetAll()
-    {
-        var list = await _service.GetAllAsync();
-        return Ok(list);
-    }
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
 
-    // GET: api/customer/{id}
-    [HttpGet("{id:int}")]
-    public async Task<ActionResult<CustomerDto>> GetById(int id)
-    {
-        var dto = await _service.GetByIdAsync(id);
-        if (dto == null) return NotFound();
-        return Ok(dto);
-    }
+        // GET: api/customer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerDto>>> GetAll()
+        {
+            var list = await _service.GetAllAsync();
+            return Ok(list);
+        }
 
-    // POST: api/customer
-    [HttpPost]
-    public async Task<ActionResult<CustomerDto>> Create([FromBody] CreateCustomerDto dto)
-    {
-        if (!ModelState.IsValid) 
-            return BadRequest(ModelState);
+        // GET: api/customer/{id}
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CustomerDto>> GetById(int id)
+        {
+            var dto = await _service.GetByIdAsync(id);
+            if (dto == null) 
+                return NotFound();
+            return Ok(dto);
+        }
 
-        var created = await _service.CreateAsync(dto);
-        return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        // POST: api/customer
+        [HttpPost]
+        public async Task<ActionResult<CustomerDto>> Create([FromBody] CreateCustomerDto dto)
+        {
+            if (!ModelState.IsValid) 
+                return BadRequest(ModelState);
+
+            var created = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
     }
 }
-
